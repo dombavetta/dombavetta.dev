7484d53e1c317646c906882479666ad160559bc1
dombavetta <dombavetta@gmail.com>
2024-11-08 16:36:28 -0600

    chore: add head title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   app: {
     head: {
+      title: 'dominic, inc',
       link: [{ rel: 'icon', href: 'https://fav.farm/💩' }]
     }
   }
