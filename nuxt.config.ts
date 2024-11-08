54d749ca3c65069ae673b82bec8a6435142c54ec
dombavetta <dombavetta@gmail.com>
2024-11-08 16:34:45 -0600

    chore: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      link: [{ rel: 'icon', href: 'https://fav.farm/💩' }]
+    }
+  }
 })
